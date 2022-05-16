5f64130f680e1fa44b0115c7ef92ccc25e81903b
wonbbnote <wonbb3313@gmail.com>
2022-05-16 11:59:53 +0900

    1강 거북이반 필수 과제 완성

---
+++ api.js
@@ -0,0 +1,13 @@
+async function handleSignin(){
+    const signupData = {
+        email : document.getElementById("floatingInput").value,
+        password : document.getElementById("floatingPassword").value
+    }
+    
+
+    const response = await fetch('http://127.0.0.1:5000/signup',{
+        method: 'POST',
+        body:JSON.stringify(signupData)
+    })
+    console.log(response)
+}
