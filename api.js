f56e97cb4b7ca18ffe6ef9bdd8a21219126f9055
wonbbnote <wonbb3313@gmail.com>
2022-05-18 01:09:55 +0900

    거북이반 2강_로그인 함수에서 로컬 스토리지에 토큰저장, 그 토큰 get 해서 이메일 페이지에 띄우기

--- api.js
+++ api.js
@@ -1,13 +1,70 @@
+const backend_base_url = "http://127.0.0.1:5000"
+const frontend_base_url = "http://127.0.0.1:5500"
+
+
 async function handleSignin(){
     const signupData = {
         email : document.getElementById("floatingInput").value,
         password : document.getElementById("floatingPassword").value
     }
     
-
-    const response = await fetch('http://127.0.0.1:5000/signup',{
-        method: 'POST',
-        body:JSON.stringify(signupData)
+    const response = await fetch(`${backend_base_url}/signup`,{
+    method: 'POST',
+    body:JSON.stringify(signupData)
     })
+
+    response_json = await response.json()
+    console.log(response_json)
+    
+
+    if (response.status == 200){
+        window.location.replace(`${frontend_base_url}/login.html`)
+        }else{
+            alert(response.status)
+        }
+}
+
+
+async function handleLogin(){
+    console.log("handle login")
+
+    const loginData = {
+        email : document.getElementById("floatingInput").value,
+        password : document.getElementById("floatingPassword").value
+    }
+
+    const response = await fetch(`${backend_base_url}/login`,{
+    method: 'POST',
+    body:JSON.stringify(loginData)
+    })
+    
     console.log(response)
+    response_json = await response.json()
+    console.log(response_json)
+    localStorage.setItem("token", response_json.token)
+    // 브라우저 자체 url에 로컬 스토리지에 저장
+
+    if (response.status == 200){
+        window.location.replace(`${frontend_base_url}`)
+    }else{
+        alert(response.status)
+    }
+}
+
+
+async function getName(){
+    console.log("get Name")
+    console.log(localStorage.getItem("token"))
+
+    const response = await fetch(`${backend_base_url}/getuserinfo`,{
+        headers:{
+            'Authorization': localStorage.getItem("token")
+        }
+        })
+        response_json = await response.json()
+        console.log(response_json)
+
+        const username = document.getElementById("username")
+        username.innerText = response_json.email
+
 }
---
+++ index.js
@@ -0,0 +1,2 @@
+console.log("index.jsdlqsleke")
+getName();
